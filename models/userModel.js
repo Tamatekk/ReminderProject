b133c5fac80eba1da59967c5444287f5d3bec8bb
Di Gao <d986000777@gmail.com>
2021-04-21 22:08:32 -0700

    push all my code so far

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+let database = require("../database");
+
+const userModel = {
+  findOne: (email) => {
+    const user = database.find((user) => user.email === email);
+    if (user) {
+      return user;
+    }
+    throw new Error(`Couldn't find user with email: ${email}`);
+  },
+};
+
+module.exports = { userModel };
