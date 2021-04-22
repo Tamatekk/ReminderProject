190bb28346d482af84f40e9b1bb66e7ec7c68003
Di Gao <d986000777@gmail.com>
2021-04-22 11:33:40 -0700

    Update database.js
    Fixe Github conflict between me and Tama's code

--- database.js
+++ database.js
@@ -1,22 +1,18 @@
 let Database = {
     cindy: {
-<<<<<<< HEAD
         email:"cindy@bcit.ca",
         password:"cindy",
         id: 6666,
-        reminders: [{id: 1, title: "Cindy First Reminder", description: "Hello", completed: false},
-                    {id: 2, title: "Cindy Second Reminder", description: "World", completed: false},   
+        reminders: [{id: 1, title: "Cindy First Reminder", description: "Hello", completed: false,subtask: ["test"]},
+                    {id: 2, title: "Cindy Second Reminder", description: "World", completed: false,subtask: ["test"]},   
                     ],
-=======
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false, subtask: ["test"]}]
->>>>>>> f9fcf4ec64e698ceb1f31c4f27658394e49de986
     },
     alex: {
         email:"alex@bcit.ca",
         password:"alex",
         id: 7777,
-        reminders: [[{id: 1, title: "Alex First Reminder", description: "Hi", completed: false},
-        {id: 2, title: "Alex Second Reminder", description: "BCIT", completed: false},   
+        reminders: [[{id: 1, title: "Alex First Reminder", description: "Hi", completed: false,subtask: ["test"]},
+        {id: 2, title: "Alex Second Reminder", description: "BCIT", completed: false,subtask: ["test"]},   
         ],]
     } 
 }
